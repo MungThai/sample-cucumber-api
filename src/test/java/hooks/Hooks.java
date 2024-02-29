3b4c6f6d18b751ea47d9e5663ae3456b820353d0
MungThai <mung.thai@outlook.com>
2024-02-29 15:40:04 -0500

    updated

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -3,14 +3,16 @@ package hooks;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import java.io.IOException;
+//import java.io.IOException;
 
 public class Hooks {
     @Before(order=40)
-    public void startUp(Scenario scenario) throws IOException {
+    public void startUp(Scenario scenario) //throws IOException
+    {
     }
 
     @After(order=40)
-    public void closeDown(Scenario scenario) throws InterruptedException {
+    public void closeDown(Scenario scenario) //throws InterruptedException {
+    {
     }
 }
